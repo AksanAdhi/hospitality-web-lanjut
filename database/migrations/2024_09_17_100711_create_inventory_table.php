e3ce8e31c3fea8c4a41a7b25eedb7f5cbec84ab0
AksanAdhi <aksanadhipratama.19@gmail.com>
2024-10-28 10:06:31 +0700

    migrationlagi

--- database/migrations/2024_09_17_100711_create_inventory_table.php
+++ database/migrations/2024_09_17_100711_create_inventory_table.php
@@ -6,30 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateInventoryTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('inventory', function (Blueprint $table) {
-            $table->id(); 
-            $table->string('name'); 
-            $table->string('type'); 
-            $table->integer('quantity');
-            $table->string('location'); 
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->integer('quantity')->default(0);
+            $table->string('location');
             $table->string('status');
             $table->date('expiration_date')->nullable();
-            $table->timestamps(); 
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('inventory');
