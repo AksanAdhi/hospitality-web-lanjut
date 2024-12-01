0865d29683930bec8ad0c93cb48a0eb200ebca16
unknown <rezak5704@gmail.com>
2024-12-01 21:03:44 +0700

    route update

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,23 @@
 <?php
 
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
 
 /*
-
-|--------------------------------------------------------------------------
-
+|---------------------------------------------------------------------------
 | Web Routes
-
-|--------------------------------------------------------------------------
-
+|---------------------------------------------------------------------------
 |
-
-| Here is where you can register web routes for your application. These
-
-| routes are loaded by the RouteServiceProvider and all of them will
-
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-
 |
-
 */
 
 Route::get('/', function () {
@@ -43,17 +38,6 @@ Route::post('/register', [RegisterController::class, 'register']);
 
 // Rute LogOut
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-// Home Route
-Route::get('/home', function() {
-    // Redirect to the home page or return a view
-    return view('home');
-});
-
-// Rute untuk navbar
-Route::get('/admin/home', function () {
-    return view('home', ['title' => 'Home']);
-});
 Route::get('/admin/appointment', function () {
     return view('appointment', ['title' => 'Janji Temu Dokter']);
 });
@@ -64,3 +48,48 @@ Route::get('/admin/pasien', function () {
     return view('pasien', ['title' => 'Pasien']);
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+// Middleware untuk admin
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/home', [AdminController::class, 'home'])->name('admin.home');
+    Route::resource('/admin/patients', PatientController::class);
+    Route::resource('/admin/inventory', InventoryController::class);
+    Route::resource('/admin/appointments', AppointmentController::class);
+    Route::resource('/admin/inventory', InventoryController::class);
+    Route::get('/admin/inventory/create', [InventoryController::class, 'create'])->name('admin.inventory.create');
+
+    // Rute untuk halaman Pelajari Lebih Lanjut
+    Route::get('/admin/learn-more', function () {
+        return view('admin.learn-more', ['title' => 'Pelajari Lebih Lanjut']);
+    })->name('admin.learn-more');
+
+    // Route About
+    Route::get('/admin/about', function () {
+        return view('admin.about', ['title' => 'Tentang Sistem']);
+    })->name('admin.about');
+});
+
+// Route untuk dokter
+Route::middleware(['auth', 'role:user'])->group(function () {
+
+    Route::get('/doctor/patients', [PatientController::class, 'indexForDoctor'])->name('doctor.patients.index');
+
+    Route::get('/profile', [DoctorController::class, 'editProfile'])->name('doctor.profile.edit');
+    Route::post('/profile', [DoctorController::class, 'updateProfile'])->name('doctor.profile.update');
+    // Halaman untuk ubah password
+    Route::get('/password/change', [DoctorController::class, 'showChangePasswordForm'])->name('doctor.password.change');
+    Route::post('/password/change', [DoctorController::class, 'updatePassword'])->name('doctor.password.update');
+
+
+    Route::get('/home', [DoctorController::class, 'home'])->name('doctor.home');
+    Route::get('/patients/{id}', [DoctorController::class, 'viewPatient'])->name('doctor.patients.show');
+    Route::get('/appointments', [DoctorController::class, 'appointments'])->name('doctor.appointments.index');
+    Route::get('doctor/appointments/{appointment}', [AppointmentController::class, 'showForDoctor'])->name('doctor.appointments.show');
+});
